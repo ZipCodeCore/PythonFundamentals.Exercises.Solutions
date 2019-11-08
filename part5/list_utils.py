1ed4fa73e9f641c7cd926860f265c1af82479c63
Roberto DeDeus Jr <roberto.p.dedeus@gmail.com>
2019-11-08 13:15:57 -0500

    Add solutions to list_utils in part 5.

---
+++ part5/list_utils.py
@@ -0,0 +1,111 @@
+from typing import List
+from math import ceil
+
+
+def get_item_at_position(list_in: List, pos: int) -> List:
+    """
+    Returns the item at pos.
+
+    :param list_in: Input list
+    :param pos: Position of desired item in list_in
+    :return: Item in pos
+    """
+    return list_in[pos]
+
+
+def print_list_items(list_in: List) -> None:
+    """
+    Given a list, this function iterates through it and prints each element.
+
+    :param list_in: Input list
+    :return: None
+    """
+    for item in list_in:
+        print(item)
+
+
+def sort_by_commit_count(list_in: List) -> List:
+    """
+    Given a list of entries, return a new list sorted based on the commit count.
+
+    :param list_in: A list where each entry is a list containing a name and the commit count corresponding to a user
+    :return: The same list sorted in ascending order based on the commit count
+    """
+    return sorted(list_in, key=lambda entry: entry[1])
+
+
+def gen_list_of_nums(n: int) -> List[int]:
+    """
+    Given a number (N), this function returns a list of integers from 0 to N (exclusive).
+
+    :param n: The number of items the result should contain
+    :return: A list of integers
+    """
+    result = []
+    for i in range(n):
+        result.append(i)
+    return result
+
+
+def half_list(list_in: List, half: int) -> List:
+    """
+    Given a list, this function will return a new list that contains half of the items in the list_in parameter.
+
+    :param list_in: The list which will be used to generate the return value
+    :param half: 1 will use the first half of the input list. 2 will use the second half of the input list.
+    If the length of list_in is an odd number, round the half value up (hint: math.ceil()).
+    :return: A list.
+    """
+    list_len = len(list_in)
+    result_len = ceil(list_len / 2)
+    result = []
+    if half == 1:
+        result = list_in[:result_len]
+    else:
+        result = list_in[result_len * -1:]
+    return result
+
+
+def remove_odds(list_in: List[int]) -> None:
+    """
+    Given a list of integers, this function removes the odd numbers from the same list.
+
+    :return: None
+    """
+    for item in list_in:
+        if item % 2 != 0:
+            list_in.remove(item)
+
+
+def remove_evens(list_in: List[int]) -> None:
+    """
+    Given a list of integers, this function removes the even numbers from the same list.
+
+    :return: None
+    """
+    for item in list_in:
+        if item % 2 == 0:
+            list_in.remove(item)
+
+
+def concatenate_lists(list_a: List, list_b: List) -> List:
+    """
+    Given two lists, this function combines them and returns the result as a new list.
+
+    :param list_a: A list
+    :param list_b: Another list
+    :return: A list containing all elements from list_a and list_b
+    """
+    return list_a + list_b
+
+
+def multiply_list(list_in: List, scalar: int) -> List:
+    """
+    Given a list and an integer, this function will return a new list which is the result of multiplying
+    the input list by the value of the scalar.
+
+    :param list_in: A list
+    :param scalar: An integer
+    :return: A list
+    """
+    return list_in * scalar
