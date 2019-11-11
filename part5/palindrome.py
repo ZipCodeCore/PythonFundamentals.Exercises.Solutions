b8dc61ad1d91fe45d5c6ec3249fd14b09aa8c308
rpdedeus <roberto.p.dedeus@gmail.com>
2019-11-11 16:37:33 -0500

    Add solution to palindrome problem.

---
+++ part5/palindrome.py
@@ -0,0 +1,14 @@
+def is_palindrome(value: str) -> bool:
+    """
+    This function determines if a word or phrase is a palindrome
+
+    :param value: A string
+    :return: A boolean
+    """
+    sanitized_input = value.lower().replace(" ", "")
+    reversed_sanitized_input = reversed(sanitized_input)
+
+    a = list(sanitized_input)
+    b = list(reversed_sanitized_input)
+
+    return a == b
