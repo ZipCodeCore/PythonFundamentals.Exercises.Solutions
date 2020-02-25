94696f9b617743e0fb2c0f3d226754fe2854de3c
rpdedeus <roberto.p.dedeus@gmail.com>
2020-02-25 17:46:57 -0500

    Update instructions for Part 10. Add solution.

---
+++ part10/small_town_teller.py
@@ -0,0 +1,70 @@
+from typing import Dict
+
+
+class Person:
+
+    def __init__(self, person_id, first_name, last_name):
+        self.id: int = person_id
+        self.first_name: str = first_name
+        self.last_name: str = last_name
+
+    def __str__(self):
+        return f"id: {self.id}. owner: {self.first_name} {self.last_name}"
+
+
+class Account:
+
+    def __init__(self, account_number, account_type, account_owner):
+        self.number: int = account_number
+        self.type: str = account_type
+        self.owner: Person = account_owner
+        self.balance: float = 0
+
+    def __str__(self):
+        return f"number: {self.number}. type: {self.type}. balance: {self.balance}"
+
+
+class Bank:
+
+    def __init__(self):
+        self.customers: Dict[int, Person] = dict()
+        self.accounts: Dict[int, Account] = dict()
+
+    def add_customer(self, customer: Person) -> None:
+        if customer.id in self.customers:
+            raise ValueError(f"Customer with id {customer.id} already exists.")
+        else:
+            self.customers[customer.id] = customer
+
+    def add_account(self, account: Account):
+        if account.owner.id not in self.customers:
+            raise ValueError(f"{account.owner.id} is not a valid customer id.")
+        elif account.number in self.accounts:
+            raise ValueError(f"Account with id {account.number} already exists")
+        else:
+            self.accounts[account.number] = account
+
+    def remove_account(self, account_id: int):
+        if account_id in self.accounts:
+            del self.accounts[account_id]
+        else:
+            raise ValueError(f"Account number {account_id} is invalid.")
+
+    def deposit(self, account_id: int, amount: float):
+        if account_id in self.accounts:
+            account = self.accounts.get(account_id)
+            account.balance += amount
+        else:
+            raise ValueError(f"Account with id {account_id} does not exist.")
+
+    def withdrawal(self, account_id, amount: float):
+        if account_id in self.accounts:
+            account = self.accounts.get(account_id)
+            account.balance -= amount
+
+    def balance_inquiry(self, account_id: int):
+        if account_id in self.accounts:
+            return self.accounts.get(account_id).balance
+        else:
+            raise ValueError(f"Account with id {account_id} does not exist.")
+
