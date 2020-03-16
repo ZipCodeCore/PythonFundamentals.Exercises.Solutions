e0131be56b05cbfe28edd4c22534808d7bfed90c
rpdedeus <roberto.p.dedeus@gmail.com>
2020-03-16 15:06:27 -0400

    Continue adding unit tests to solution in part 10.

--- part10/test_small_town_teller.py
+++ part10/test_small_town_teller.py
@@ -5,8 +5,12 @@ import small_town_teller
 class SmallTownTellerTest(unittest.TestCase):
 
     def setUp(self) -> None:
-        self._customer = john = small_town_teller.Person(1, 'John', 'Smith')
-        self._account = small_town_teller.Account(123, 'CHECKING', john)
+        self._john = small_town_teller.Person(1, 'John', 'Smith')
+        self._jane = small_town_teller.Person(2, 'Jane', 'Doe')
+        self._walter = small_town_teller.Person(1, 'Walter', 'White')
+        self._john_checking = small_town_teller.Account(123, 'CHECKING', self._john)
+        self._jane_savings = small_town_teller.Account(456, 'SAVINGS', self._jane)
+        self._walter_checking = small_town_teller.Account(123, 'CHECKING', self._walter)
         self._bank = small_town_teller.Bank()
 
     def test_person_initialization(self):
@@ -14,9 +18,9 @@ class SmallTownTellerTest(unittest.TestCase):
         expected_first_name = 'John'
         expected_las_name = 'Smith'
 
-        self.assertEqual(expected_id, self._customer.id)
-        self.assertEqual(expected_first_name, self._customer.first_name)
-        self.assertEqual(expected_las_name, self._customer.last_name)
+        self.assertEqual(expected_id, self._john.id)
+        self.assertEqual(expected_first_name, self._john.first_name)
+        self.assertEqual(expected_las_name, self._john.last_name)
 
     def test_account_initialization(self):
         expected_number = 123
@@ -24,27 +28,41 @@ class SmallTownTellerTest(unittest.TestCase):
         expected_owner = 'id: 1. owner: John Smith'
         expected_balance = 0
 
-        self.assertEqual(expected_number, self._account.number)
-        self.assertEqual(expected_type, self._account.type)
-        self.assertEqual(expected_owner, str(self._account.owner))
-        self.assertEqual(expected_balance, self._account.balance)
+        self.assertEqual(expected_number, self._john_checking.number)
+        self.assertEqual(expected_type, self._john_checking.type)
+        self.assertEqual(expected_owner, str(self._john_checking.owner))
+        self.assertEqual(expected_balance, self._john_checking.balance)
 
     def test_bank_initialization(self):
         self.assertEqual(dict(), self._bank.accounts)
         self.assertEqual(dict(), self._bank.customers)
 
     def test_bank_add_customer(self):
-        jane = small_town_teller.Person(2, 'Jane', 'Doe')
-        self._bank.add_customer(jane)
+        self._bank.add_customer(self._jane)
 
         self.assertEqual(1, len(self._bank.customers))
-        self.assertEqual(jane.id, self._bank.customers.get(2).id)
-        self.assertEqual(jane.first_name, self._bank.customers.get(2).first_name)
-        self.assertEqual(jane.last_name, self._bank.customers.get(2).last_name)
+        self.assertEqual(self._jane.id, self._bank.customers.get(2).id)
+        self.assertEqual(self._jane.first_name, self._bank.customers.get(2).first_name)
+        self.assertEqual(self._jane.last_name, self._bank.customers.get(2).last_name)
 
     def test_bank_add_customer_duplicate_id(self):
-        jane = small_town_teller.Person(1, 'Jane', 'Doe')
-        self._bank.add_customer(jane)
+        self._bank.add_customer(self._walter)
 
         with self.assertRaises(ValueError):
-            self._bank.add_customer(self._customer)
+            self._bank.add_customer(self._john)
+
+    def test_bank_add_account(self):
+        self._bank.add_customer(self._john)
+        self._bank.add_account(self._john_checking)
+
+        self.assertEqual(1, len(self._bank.accounts))
+        self.assertEqual(self._john, self._bank.accounts.get(123).owner)
+
+    def test_bank_add_account_duplicate_id(self):
+        self._bank.add_customer(self._john)
+        self._walter.id = 11
+        self._bank.add_customer(self._walter)
+        self._bank.add_account(self._john_checking)
+
+        with self.assertRaises(ValueError):
+            self._bank.add_account(self._walter_checking)
