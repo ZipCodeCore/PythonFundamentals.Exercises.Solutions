d2c26894463b0a05aadcdde7a42b8f03efe3e6f2
rpdedeus <roberto.p.dedeus@gmail.com>
2020-03-16 23:09:06 -0400

    Update type hints and docstrings for part 12 solutions.

--- part12/viva_comprehensions.py
+++ part12/viva_comprehensions.py
@@ -1,4 +1,4 @@
-from typing import List, Callable
+from typing import List, Dict, Set, Callable
 import enum
 
 
@@ -8,6 +8,16 @@ class Parity(enum.Enum):
 
 
 def gen_list(start: int, stop: int, parity: Parity) -> List[int]:
+    """
+    Oh no some evil developer decided not to write docstrings. Maybe you can use the test cases to decipher
+    what this method was supposed to do. Hey if you do, maybe you could do some good in this world by
+    updating this here docstring to something useful.
+
+    :param start:
+    :param stop:
+    :param parity:
+    :return:
+    """
     if parity == parity.ODD:
         result = [i for i in range(start, stop) if i % 2 != 0]
     elif parity == parity.EVEN:
@@ -17,9 +27,27 @@ def gen_list(start: int, stop: int, parity: Parity) -> List[int]:
     return result
 
 
-def gen_dict(start: int, stop: int, strategy: Callable):
+def gen_dict(start: int, stop: int, strategy: Callable) -> Dict:
+    """
+    Oh no some evil developer decided not to write docstrings. Maybe you can use the test cases to decipher
+    what this method was supposed to do. Hey if you do, maybe you could do some good in this world by
+    updating this here docstring to something useful.
+
+    :param start:
+    :param stop:
+    :param strategy:
+    :return:
+    """
     return {i: strategy(i) for i in range(start, stop)}
 
 
-def gen_set(val_in: str):
+def gen_set(val_in: str) -> Set:
+    """
+    Oh no some evil developer decided not to write docstrings. Maybe you can use the test cases to decipher
+    what this method was supposed to do. Hey if you do, maybe you could do some good in this world by
+    updating this here docstring to something useful.
+
+    :param val_in:
+    :return:
+    """
     return {c.upper() for c in set(val_in) if c.islower()}
